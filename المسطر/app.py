b85191d0f7a33cbb7bdc923103f31fa9fc7a3801
hishamalhansh-cmd <hishamalhansh@gmail.com>
2026-03-12 23:59:20 +0300

    Update app.py

--- المسطر/app.py
+++ المسطر/app.py
@@ -2330,4 +2330,5 @@ def edit_profile():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
